6b8278eb367135e262b2646b226aaa42b6a790d6
Lucas <lmferreira@indra.es>
2018-07-22 11:14:41 +0200

    MOdificados errores de visualización y listado

--- src/app/modules/players/players.component.ts
+++ src/app/modules/players/players.component.ts
@@ -17,6 +17,12 @@ export class PlayersComponent implements OnInit {
 
   filterargs:any;
 
+  private withOutNameTeam: string = "Sin Equipo";
+  private withOutNamePlayer: string = "Sin Nombre";
+  private withOutNameMationality: string = "Sin Nacionalidad";
+  private withOutPhoto: string = "../../../assets/images/sinJugador.png";
+  private withOutPosition: string = "Sin Demarcación";
+  private withOutSecondname: string = "";
 
   constructor(private playersService : PlayersService, private modelTeam: Team, public spinner: NgxSpinnerService, public router: ActivatedRoute) {
 
@@ -37,52 +43,9 @@ export class PlayersComponent implements OnInit {
   private getAllPlayers(){
 
     this.playersService.getPlayers().subscribe(result =>{
-      this.dataPlayers = result.data;
+      //this.dataPlayers = result.data;
 
-      console.log('Jugadores', result.data)
-      let array =[];
-      let object ={}
-
-      let nameTeam = [];
-
-      for(let i =0 ; i<result.data.length; i++){
-        if(result.data[i].team === null){
-          nameTeam = [
-            {
-              "name" :"Sin equipo",
-              "_id": 0
-            }
-          ]
-        }else{
-          nameTeam = [{
-
-              "name": result.data[i].team[0].name,
-             "_id": result.data[i].team[0]._id
-           }]
-
-        }
-
-        object ={
-          "name": result.data[i].name,
-          "nationality": result.data[i].nationality,
-          "photo": result.data[i].photo,
-          "position": result.data[i].position,
-          "secondname": result.data[i].secondname,
-          "team": nameTeam,
-          "_id":result.data[i]._id,
-          "characteristics":{
-            "age" : result.data[i].characteristics.age,
-            "height" : result.data[i].characteristics.height,
-            "weight" : result.data[i].characteristics.weight
-          }
-        }
-
-        array.push(object);
-      }
-
-      console.log('Jugadores...',array)
-
-      this.dataPlayers = array;
+      this.prepareObject(result);
 
       if(this.teamFilter ==='AllTeams'){
         this.showPlayersForTeam(0);
@@ -93,8 +56,125 @@ export class PlayersComponent implements OnInit {
     })
   }
 
+  /**
+   *
+   * @param team
+   */
   showPlayersForTeam (team){
      this.filterargs = {'idEquipo' : team}
+  }
+
+  /**
+   *
+   * @param result
+   */
+  prepareObject(result){
+    console.log('Jugadores', result.data)
+    let array =[];
+
+    let nameTeam = [];
+
+    for(let i =0 ; i<result.data.length; i++){
+
+      let object = {
+        "name": '',
+        "nationality": '',
+        "photo": '',
+        "position": '',
+        "secondname": '',
+        "team": [],
+        "_id":result.data[i]._id,
+        "characteristics":{
+          "age" : '',
+          "height" : '',
+          "weight" :''
+        }
+      }
+
+
+      if(result.data[i].team!==undefined && result.data[i].team!=="" && result.data[i].team!==null){
+        nameTeam = [
+          {
+            "name" : result.data[i].team.name,
+            "_id": result.data[i].team._id
+          }
+        ]
+      }else{
+        nameTeam = [
+              {
+                "name" : this.withOutNameTeam,
+                "_id": 0
+              }
+        ]
+      }
+      object.team =nameTeam;
+
+      if(result.data[i].name!==undefined && result.data[i].name!=="" && result.data[i].name!==null){
+        object.name = result.data[i].name;
+      }else{
+        object.name = this.withOutNamePlayer;
+      }
+
+      if(result.data[i].nationality!==undefined && result.data[i].nationality!=="" && result.data[i].nationality!==null){
+        object.nationality = result.data[i].nationality;
+      }else{
+        object.nationality = this.withOutNameMationality;
+      }
+
+
+      if(result.data[i].photo!==undefined && result.data[i].photo!=="" && result.data[i].photo!==null){
+        object.photo = result.data[i].photo;
+      }else{
+        object.photo = this.withOutPhoto;
+      }
+
+
+      if(result.data[i].position!==undefined && result.data[i].position!=="" && result.data[i].position!==null){
+        object.position = result.data[i].position;
+      }else{
+        object.position = this.withOutPosition;
+      }
+
+
+      if(result.data[i].secondname!==undefined && result.data[i].secondname!=="" && result.data[i].secondname!==null){
+        object.secondname = result.data[i].secondname;
+      }else{
+        object.secondname = this.withOutSecondname;
+      }
+
+
+      if(result.data[i].characteristics!==undefined && result.data[i].characteristics!=="" && result.data[i].characteristics!==null){
+
+        if(result.data[i].characteristics.age!==undefined && result.data[i].characteristics.age!=="" && result.data[i].characteristics.age!==null){
+          object.characteristics.age =result.data[i].characteristics.age;
+        }else{
+          object.characteristics.age = "";
+        }
+
+        if(result.data[i].characteristics.height!==undefined && result.data[i].characteristics.height!=="" && result.data[i].characteristics.height!==null){
+          object.characteristics.height =result.data[i].characteristics.height;
+        }else{
+          object.characteristics.height = "";
+        }
+
+        if(result.data[i].characteristics.weight!==undefined && result.data[i].characteristics.weight!=="" && result.data[i].characteristics.weight!==null){
+          object.characteristics.weight =result.data[i].characteristics.weight;
+        }else{
+          object.characteristics.weight = "";
+        }
+
+      }else{
+        object.characteristics.age = "";
+        object.characteristics.height = "";
+        object.characteristics.weight = "";
+      }
+
+      array.push(object);
+    }
+
+    console.log('Jugadores...',array)
+
+    this.dataPlayers = array;
   }
 
 }
--- src/app/modules/teams/list-teams/list-teams.component.ts
+++ src/app/modules/teams/list-teams/list-teams.component.ts
@@ -14,6 +14,13 @@ export class ListTeamsComponent implements OnInit {
 
   public dataTeams: any;
 
+  private withOutName : string = "Sin nombre de equipo"
+  private withOutStadium : string ="Sin estadio"
+  private withOutCoach : string = "Sin entrenador"
+  private withOutPresident :string = "Sin presidente"
+  private withOutShield: string = '../../../../assets/images/sin_escudo.png';
+
+
   constructor(private teamsService: TeamsService,
               private spinner: NgxSpinnerService,
               private modelTeam: Team,
@@ -26,8 +33,7 @@ export class ListTeamsComponent implements OnInit {
 
   private getListTeams(){
     this.teamsService.getTeams().subscribe(result=>{
-      this.spinner.hide();
-      this.dataTeams = result.data;
+      this.configureData(result.data);
     })
   }
 
@@ -36,4 +42,83 @@ export class ListTeamsComponent implements OnInit {
     this.router.navigate(['/players',team]);
   }
 
+  /**
+   * @description Preparamos la configuración del array para pintar aunque no vengan datos
+   * @param data
+   */
+  configureData(data){
+
+    let array=[];
+
+    for(let i=0; i<data.length; i++){
+
+      let objectTeam = {
+        "_id":data[i]._id,
+        "name":'',
+        "stadium":'',
+        "coach":"",
+        "president":"",
+        "shield":"",
+        "history":{
+          "titles":"",
+          "goals":""
+        }
+      };
+
+      if(data[i].name!==undefined && data[i].name!=="" && data[i].name!==null){
+        objectTeam.name = data[i].name;
+      }else{
+        objectTeam.name = this.withOutName;
+      }
+
+      if(data[i].stadium!==undefined && data[i].stadium!=="" && data[i].stadium!==null){
+        objectTeam.stadium = data[i].stadium;
+      }else{
+        objectTeam.stadium = this.withOutStadium;
+      }
+
+      if(data[i].coach!==undefined && data[i].coach!=="" && data[i].coachnull){
+        objectTeam.coach = data[i].coach;
+      }else{
+        objectTeam.coach = this.withOutCoach;
+      }
+
+      if(data[i].president!==undefined && data[i].president!=="" && data[i].president!==null){
+        objectTeam.name = data[i].name;
+      }else{
+        objectTeam.name = this.withOutPresident;
+      }
+
+      if(data[i].shield!==undefined && data[i].shield!=="" && data[i].shield!==null){
+        objectTeam.shield = data[i].shield;
+      }else{
+        objectTeam.shield = this.withOutShield;
+      }
+
+      if(data[i].history!==undefined && data[i].history!=="" && data[i].history!==null){
+
+        if(data[i].history.goals!==undefined && data[i].history.goals!=="" && data[i].history.goals!==null){
+          objectTeam.history.goals = data[i].history.goals
+        }else{
+          objectTeam.history.goals = '';
+        }
+
+        if(data[i].history.titles!==undefined && data[i].history.titles!=="" && data[i].history.titles!==null){
+          objectTeam.history.titles = data[i].history.titles
+        }else{
+          objectTeam.history.titles = '';
+        }
+
+
+      }else{
+        objectTeam.shield = this.withOutShield;
+      }
+
+      array.push(objectTeam);
+    }
+
+    this.dataTeams = array;
+
+    this.spinner.hide();
+  }
 }
